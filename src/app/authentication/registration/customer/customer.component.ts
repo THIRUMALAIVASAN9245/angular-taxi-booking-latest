40b9b650fabe465811244710fad099440d53c92f
THIRUMALAIVASAN9245 <thirumalaivasan9245@gmail.com>
2019-10-06 22:46:19 +0530

    1 file modified, 1 added

--- src/app/authentication/registration/customer/customer.component.ts
+++ src/app/authentication/registration/customer/customer.component.ts
@@ -1,14 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { MustMatch } from '../must-match.validator';
 
 @Component({
-    selector: 'app-customer',
-    templateUrl: './customer.component.html'
+  selector: 'app-customer',
+  templateUrl: './customer.component.html'
 })
 export class CustomerComponent implements OnInit {
+  registerForm: FormGroup;
+  submitted = false;
 
-    constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-    ngOnInit(): void {
-       
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      gender: ['', Validators.required],
+      phoneNo: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: MustMatch('password', 'confirmPassword')
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
     }
+
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value))
+  }
 }
