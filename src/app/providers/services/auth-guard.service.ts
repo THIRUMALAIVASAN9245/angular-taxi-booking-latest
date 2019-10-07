ab9f636cb418181463cac81a00f0da398aa89a29
THIRUMALAIVASAN9245 <thirumalaivasan9245@gmail.com>
2019-10-08 02:46:27 +0530

    2 files modified

--- src/app/providers/services/auth-guard.service.ts
+++ src/app/providers/services/auth-guard.service.ts
@@ -4,14 +4,14 @@ import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router, private authService: AuthService) { }
-  canActivate() {
-    // if (!this.authService.isTokenExpired()) {           
-    // logged in so return true
-    return true;
-    // }
-    // not logged in so redirect to login page
-    // this.router.navigate(['/login']);
-    // return false;
-  }
+    constructor(private router: Router, private authService: AuthService) { }
+    canActivate() {            
+        if (!this.authService.isTokenExpired()) {           
+            // logged in so return true
+            return true;
+        }
+        // not logged in so redirect to login page
+        this.router.navigate(['/auth/login']);
+        return false;
+    }
 }
